1b8dbaa77d45f8c473420e050ea284797d40fb8c
kperkins97 <perkins.kelsey@yahoo.com>
2021-10-03 11:25:23 -0500

    trying to display weather

--- assets/script.js
+++ assets/script.js
@@ -18,92 +18,68 @@ var formSubmit = function (event) {
   } else {
     alert('Please enter a valid city name');
   }
+  if (cityname) {
+    getFiveDays(cityname);
+    tempContainerEl.textContent = '';
+    cityInputEl.value = '';
+  }
 };
 
-// var btnClick = function (event) {
-//   // `event.target` is a reference to the DOM element of what programming language button was clicked on the page
-//   var language = event.target.getAttribute('data-language');
+var btnClick = function (event) {
+  // `event.target` is a reference to the DOM element of what programming language button was clicked on the page
+  var weatherCity = event.target.getAttribute('submit');
+  console.log('did this work?')
 
-//   // If there is no language read from the button, don't attempt to fetch repos
-//   if (language) {
-//     getFiveDays(language);
+  if (weatherCity) {
+    getFiveDays(weatherCity);
 
-//     repoContainerEl.textContent = '';
-//   }
-// };
+    repoContainerEl.textContent = '';
+  }
+};
 
 var getWeatherInfo = function (city) {
-  var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="  + city + "&appid=" + ApiKey;
+  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='  + city + '&appid=' + ApiKey;
 
   fetch(weatherUrl)
     .then(function (response) {
       if (response.ok) {
-        //console.log(response);
+        console.log(response);
         response.json().then(function (data) {
-          //console.log(data);
+          console.log(data);
           displayWeather(data, city);
         });
-    //   } else {
-    //     alert('Error: ' + response.statusText);
-    //   }
-    // })
-    // .catch(function (error) {
-    //   alert('Unable to connect to GitHub');
-    // });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to City');
+    });
 };
 
-var getFiveDays = function (fivedays) {
-  // The `q` parameter is what language we want to query, the `+is:featured` flag adds a filter to return only featured repositories
-  // The `sort` parameter will instruct GitHub to respond with all of the repositories in order by the number of issues needing help
-  var weatherUrl = 'https://api.github.com/search/repositories?q=' + fivedays + '+is:featured&sort=help-wanted-issues';
 
-  fetch(apiUrl).then(function (response) {
+var getFiveDays = function (fivedays) {
+
+  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='  + fivedays + '&appid=' + ApiKey;
+  fetch(weatherUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         displayWeather(data.items, fivedays);
       });
-    // } else {
-    //   alert('Error: ' + response.statusText);
-    // }
-  // });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  });
 };
+
+//NEED TO DISPLAY 5 DAY WEATHER HERE:
 
 var displayWeather = function (wx, citySearching) {
   if (wx.length === 0) {
     repoContainerEl.textContent = 'No Weather Information Found';
-    // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
     return;
   }
-
-  wxSearchTerm.textContent = citySearching;
-
-  for (var i = 0; i < wx.length; i++) {
-    // The result will be `<github-username>/<github-repository-name>`
-    var cityName = wx[i]//.owner.login + '/' + repos[i].name;
-
-    var cityEl = document.createElement('div');
-    cityEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
+  citySearch.textContent = citySearching;
 };
 
-userFormEl.addEventListener('submit', formSubmitHandler);
-//languageButtonsEl.addEventListener('click', buttonClickHandler);
+cityFormEl.addEventListener('submit', formSubmit);
